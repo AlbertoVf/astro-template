a2222261adc0b177aebeae5688205291faf79ffe
AlbertoVf <avf.git-repos@protonmail.com>
2024-09-18 16:12:40 +0200

    feat: configure tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				neutral: 'var(--color-neutral)',
+				valid: 'var(--color-valid)',
+				invalid: 'var(--color-invalid)',
+				white: 'var(--color-white)',
+				blue: 'var(--color-blue)',
+				red: 'var(--color-red)',
+				green: 'var(--color-green)',
+				grey_light: 'var(--color-grey-light)',
+				grey: 'var(--color-grey)',
+				grey_dark: 'var(--color-grey-dark)',
+			},
+			fontFamily: {
+				monospace: ['Cousine', 'Courier New', 'Courier', 'monospace'],
+				cursive: ['Kalam', 'cursive'],
+				serif: ['Bona Nova SC', 'Times New Roman', 'Times', 'serif'],
+				sans: ['Montserrat', 'Franklin Gothic Medium', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
